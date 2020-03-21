9813c6b9a3a5aede65a6f2cb42ba9e1f8c356b09
Dan Streeter <dan@flowmo.co>
2020-03-21 21:04:04 +0000

    Working implementation of serial listner with rabbit MQ push of text data.

--- gsm_module.py
+++ gsm_module.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
+# System Imports
+from datetime import (
+    datetime,
+    timezone
+)
 import json
+import pika
 import serial
-from datetime import datetime
+from time import sleep
 
+# Framework / Library Imports
+
+# Application Imports
+
+# Local Imports
 import config
-
-
-port = serial.Serial(config.serial_port, 9600, timeout=2)
-if (not port.isOpen()):
-    port.open()
+from functions import *
 
 
 def send_at_command(command, append_eol=True, encoding='iso8859_2'):
@@ -52,7 +59,7 @@ def enter_pin(pin=None):
 
 
 def send_sms_message(phone_number, text):
-    assert phone_number.startswith("+421")
+    assert phone_number.startswith("+44")
 
     command_sequence = [
         "AT+CMGF=1",
@@ -72,21 +79,19 @@ def get_sms_messages(category="ALL"):
         "ALL", "REC READ", "REC UNREAD", "STO UNSENT", "STO SENT"
     ]
 
-    result = []
+    messages = []
     response_raw = send_at_command("AT+CMGL=" + category)
 
-    print(response_raw)
+    # print(response_raw)
 
     sms_list_raw = response_raw[2:-2]
     # the odd elements are sms metadata, the even ones are sms texts
     sms_pairs = zip(sms_list_raw[0::2], sms_list_raw[1::2])
 
     for sms_meta, sms_text in sms_pairs:
-        result.append(parse_sms(sms_meta, sms_text))
+        messages.append(parse_sms(sms_meta, sms_text))
 
-    print(result)
-
-    return result
+    return messages
 
 
 def delete_all_sms_messages():
@@ -107,9 +112,26 @@ def parse_sms(sms_meta, sms_text):
         'index': int(sms_meta[0].split(': ')[1]),
         'category': sms_meta[1].split("\"")[1],
         'sender': sms_meta[2].split("\"")[1],
-        'date': sms_meta[4].split("\"")[1],
-        'text': sms_text
+        'datetime': str(format_dtstr_to_obj(clean_datetime(sms_meta[4] + " " + sms_meta[5]))),
+        'timestamp': format_dtstr_to_obj(clean_datetime(sms_meta[4] + " " + sms_meta[5])).replace(tzinfo=timezone.utc).timestamp(),
+        'text': sms_text.rstrip()
     }
+
+
+def clean_datetime(dt_string):
+    dt_string = dt_string.rstrip() # Remove the new line chars
+    if dt_string.startswith('"'):
+        dt_string = dt_string[1:] # Remove starting "
+    if dt_string.endswith('"'):
+        dt_string = dt_string[0:-1] # Remove ending "
+    if dt_string.endswith("+00"):
+        dt_string = dt_string+"00"
+
+    return dt_string
+
+
+def format_dtstr_to_obj(dt_string):
+    return datetime.strptime(dt_string, '%Y/%m/%d %H:%M:%S%z')
 
 
 def get_phonebook(begin=1, end=250):
@@ -117,7 +139,7 @@ def get_phonebook(begin=1, end=250):
     result = list(map(parse_raw_phonebook_entry, response[2:-3]))
 
     return result
-    
+
 
 def parse_raw_phonebook_entry(entry):
     entry = entry[entry.find('+CPBR: ') + 7:]
@@ -153,5 +175,81 @@ def load_phonebook_from_file(filename='contacts.json'):
         print(entry['id'])
 
 
-init(config.sim_card_pin)
-load_phonebook_from_file()
+def watch_for_unbound():
+    print("Listening to port...")
+    while True:
+        received_data = port.read()              #read serial port
+        sleep(0.03)
+        data_left = port.inWaiting()             #check for remaining byte
+        received_data += port.read(data_left)
+        if len(received_data) > 0:
+            line_data = received_data.decode("iso8859_2")
+            # print (line_data)    #print received data
+
+            if '+CIEV: "MESSAGE"' in line_data:
+                print("Inbound Message!")
+                collect_and_push_to_rabbit(get_sms_messages())
+        # port.write(received_data)                #transmit data serially 
+
+
+def get_rabbit_connection(user, password, server):
+    credentials = pika.PlainCredentials(
+        config.RABBIT_USER, 
+        config.RABBIT_PASS
+    )
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            config.RABBIT_SERVER,
+            5672,
+            '/',
+            credentials
+        )
+    )
+    return connection.channel()
+
+
+def collect_and_push_to_rabbit(messages):
+    for message in messages:
+        try:
+            # print("Timestamp: " + str(message['timestamp']))
+            # print(str(message))
+            channel.basic_publish(
+                exchange='',
+                routing_key=config.RABBIT_QUEUE,
+                body=json.dumps(message),
+                # properties=pika.BasicProperties(
+                #     content_type="application/json",
+                #     headers={
+                #             'id': header_frame.headers['id'],
+                #             'task': header_frame.headers['task']
+                #     }
+                # ),
+            )
+            delete_sms_message(message['index'])
+            print("Message handled and pushed to queue")
+        except Exception:
+            print("There was a problem")
+
+try:
+    port = serial.Serial(config.serial_port, config.baud_rate, timeout=2)
+    if (not port.isOpen()):
+        print('Opening Port')
+        port.open()
+
+    print('Initializing SIM')
+    init(config.sim_card_pin)
+
+    print('Initializing RabbitMQ Connection')
+    channel = get_rabbit_connection(
+        user = config.RABBIT_USER,
+        password = config.RABBIT_PASS,
+        server = config.RABBIT_SERVER
+    )
+    channel.queue_declare(queue=config.RABBIT_QUEUE)
+
+    # load_phonebook_from_file()
+    # send_sms_message("+447xxxxxxxxx", "Testing 123")
+    watch_for_unbound()
+
+except KeyboardInterrupt:
+    print("Quitting...")
